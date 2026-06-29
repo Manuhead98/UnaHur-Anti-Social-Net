61b2bbce96e796fd414658c28d33df336fb7fb51
Juan Manuel Solis Diaz <manuhead98@gmail.com>
2026-06-29 11:39:00 -0300

    crear post card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import PostCard from "../components/feed/PostCard";
+import CreatePostCard from "../components/feed/CreatePostCard";
 
 function Home() {
     return (
         <div className="container mx-auto px-4">
 
             <div className="max-w-2xl mx-auto space-y-6">
-
+                <CreatePostCard></CreatePostCard>
                 <PostCard />
                 <PostCard />
                 <PostCard />
